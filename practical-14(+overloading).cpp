ecf23e3ddfeec54117f8abf1c8386b5aa0039747
s-r-aman <s.r.aman.sra@gmail.com>
2018-04-17 21:16:39 +0530

    Added practical 14.

---
+++ practical-14(+overloading).cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+class Demo
+{
+  int a, b;
+
+public:
+  Demo(int, int);
+  Demo operator+(Demo);
+};
+
+Demo ::Demo(int x, int y)
+{
+  a = x;
+  b = y;
+  std::cout << "Demo object has been created with values: " << x << " & " << y << "." << std::endl;
+}
+
+Demo Demo::operator+(Demo temp)
+{
+  int x = temp.a + a;
+  int y = temp.b + b;
+  Demo c(x, y);
+  std::cout << "Addition has been done." << std::endl;
+  return c;
+}
+
+int main()
+{
+  Demo a(2, 3);
+  Demo b(8, 7);
+  Demo c = a + b;
+}
